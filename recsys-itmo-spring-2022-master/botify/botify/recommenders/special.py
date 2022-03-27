2ceac58f6ad3b01eaa3bdf39815cfc661620e1c8
Panamec1 <283091@niuitmo.ru>
2022-03-27 21:11:22 +0300

    Re-make

---
+++ recsys-itmo-spring-2022-master/botify/botify/recommenders/special.py
@@ -0,0 +1,39 @@
+import random
+from typing import List
+
+
+from .recommender import Recommender
+from .random import Random
+
+
+class Special(Recommender):
+    def __init__(self, tracks_redis, artists_redis, catalog, top_tracks):
+        self.tracks_redis = tracks_redis
+        self.artists_redis = artists_redis
+        self.catalog = catalog
+
+        self.random = Random(tracks_redis)
+        self.top_tracks = top_tracks
+
+    def recommend_next(self, user: int, prev_track: int, prev_track_time: float) -> int:
+
+        if self.top_tracks and (prev_track_time < 0.4):
+            shuffled = list(self.top_tracks)
+            random.shuffle(shuffled)
+            return shuffled[0]
+
+
+        track_data = self.tracks_redis.get(prev_track)
+        if track_data is not None:
+            track = self.catalog.from_bytes(track_data)
+        else:
+            raise ValueError(f"Track not found: {prev_track}")
+
+        artist_data = self.artists_redis.get(track.artist)
+        if artist_data is not None:
+            artist_tracks = self.catalog.from_bytes(artist_data)
+        else:
+            return self.random.recommend_next(user, prev_track, prev_track_time)
+
+        index = random.randint(0, len(artist_tracks) - 1)
+        return artist_tracks[index]
